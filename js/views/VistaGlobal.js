8fba9d2388e749f863693baf9f081150ab6a5fc5
Otto Colomina Pardo <otto@dccia.ua.es>
2015-11-24 10:44:56 +0100

    vista global auto-inicializada

--- js/views/VistaGlobal.js
+++ js/views/VistaGlobal.js
@@ -11,18 +11,26 @@ Marvel.Views = Marvel.Views || {};
             formBusqueda: "#formBusqueda",
             listado: '#listado'
         },
+        initialize: function() {
+            this.getRegion('formBusqueda').show(new Marvel.Views.VistaBuscarComics());
+        },
+        //La vista global escucha los eventos de sus hijas
         childEvents: {
+            //cuando se obtienen los resultados del API
             'completed:search' : function(child, col) {
-                console.log(col);
                 this.getRegion('listado').show(new Marvel.Views.VistaComics({
                     collection: col
                 }))
             },
+            //Cuando se ha pulsado sobre "ver detalles" en un elemento de la lista
             'show:details': function(child, model) {
                 this.vistaLista = this.getRegion('listado').currentView;
                 var nv = new Marvel.Views.VistaDetallesComic({model: model});
+                //preventDestroy evita que se libere la memoria de la vista de lista
+                //ya que nos va a servir de nuevo cuando se cierren los detalles
                 this.getRegion('listado').show(nv,{preventDestroy:true});
             },
+            //Cuando se ha pulsado sobre "cerrar detalles"
             'hide:details': function() {
                 this.getRegion('listado').show(this.vistaLista);
             }
